810dba33c900a2891c00625f720a502a9e4dfb80
s prem kishore <97545164+Premk34@users.noreply.github.com>
2024-08-13 20:00:16 +0530

    js started

---
+++ class 5/app.js
@@ -0,0 +1,14 @@
+// let myVariable = "hello world";
+// console.log(myVariable)
+
+let firstVariable;
+let secondVariable  = 2;
+// undefined
+console.log(2+2 > 2+1 == 2+5 < 0)
+// console.log(firstVariable - secondVariable)
+// 4 > 3 = true;
+// 7 < 0 = false;
+
+// 1 == 0
+// false
+
